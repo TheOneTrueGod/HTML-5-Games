a2b7c29a18f0b57544e5d9a5f313a3b04459853a
Jeremy Prevoe <jprevoe@MacBook-Pro-5.local>
2017-07-09 00:19:18 -0700

    Reflection Working in tests

--- TurnBasedGames/Bouncy/js/main.js
+++ TurnBasedGames/Bouncy/js/main.js
@@ -1,49 +1,3 @@
-function checkLineIntersection(line1StartX, line1StartY, line1EndX, line1EndY, line2StartX, line2StartY, line2EndX, line2EndY) {
-    // if the lines intersect, the result contains the x and y of the intersection (treating the lines as infinite) and booleans for whether line segment 1 or line segment 2 contain the point
-    var denominator, a, b, numerator1, numerator2, result = {
-        x: null,
-        y: null,
-        onLine1: false,
-        onLine2: false
-    };
-    denominator = ((line2EndY - line2StartY) * (line1EndX - line1StartX)) - ((line2EndX - line2StartX) * (line1EndY - line1StartY));
-    if (denominator === 0) {
-        return result;
-    }
-    a = line1StartY - line2StartY;
-    b = line1StartX - line2StartX;
-    numerator1 = ((line2EndX - line2StartX) * a) - ((line2EndY - line2StartY) * b);
-    numerator2 = ((line1EndX - line1StartX) * a) - ((line1EndY - line1StartY) * b);
-    a = numerator1 / denominator;
-    b = numerator2 / denominator;
-
-    // if we cast these lines infinitely in both directions, they intersect here:
-    result.x = line1StartX + (a * (line1EndX - line1StartX));
-    result.y = line1StartY + (a * (line1EndY - line1StartY));
-/*
-        // it is worth noting that this should be the same as:
-        x = line2StartX + (b * (line2EndX - line2StartX));
-        y = line2StartX + (b * (line2EndY - line2StartY));
-        */
-    // if line1 is a segment and line2 is infinite, they intersect if:
-    if (a > 0 && a < 1) {
-        result.onLine1 = true;
-    }
-    // if line2 is a segment and line1 is infinite, they intersect if:
-    if (b > 0 && b < 1) {
-        result.onLine2 = true;
-    }
-    // if line1 and line2 are segments, they intersect if both of the above are true
-    return result;
-}
-
-
-
-
-
-
-
-
 class MainGame {
   constructor() {
     this.ticksPerTurn = 20;
@@ -64,79 +18,46 @@ class MainGame {
     this.playerCommands = [];
   }
 
-  addLineFromTo(x1, y1, x2, y2) {
+  addLine(line, color) {
     // Create a new Graphics object and add it to the scene
     var lineGraphic = new PIXI.Graphics();
     this.stage.addChild(lineGraphic);
 
     // Move it to the beginning of the line
-    lineGraphic.position.set(x1, y1);
+    lineGraphic.position.set(line.x1, line.y1);
 
     // Draw the line (endPoint should be relative to myGraph's position)
-    lineGraphic.lineStyle(2, 0xffffff)
+    lineGraphic.lineStyle(1, color)
            .moveTo(0, 0)
-           .lineTo(x2 - x1, y2 - y1);
+           .lineTo(line.x2 - line.x1, line.y2 - line.y1);
     return lineGraphic;
   }
 
-  findIntersectPoint(x1, y1, x2, y2, lines) {
-    var closest = null;
-    for (var i = 0; i < lines.length; i++) {
-      var l2 = lines[i];
-      var intersectionPoint = checkLineIntersection(
-        x2, y2, x1, y1,
-        l2[0], l2[1], l2[2], l2[3]
-      );
-      if (intersectionPoint &&
-        intersectionPoint.onLine1 && intersectionPoint.onLine2
-      ) {
-        if (!closest) {
-          closest = {x: intersectionPoint.x, y: intersectionPoint.y};
-        } else {
-          var closestDist = Math.pow(x1 - closest.x, 2) + Math.pow(y1 - closest.y, 2);
-          var newDist = Math.pow(x1 - intersectionPoint.x, 2) + Math.pow(x2 - intersectionPoint, 2);
-          if (newDist < closestDist) {
-            closest = {x: intersectionPoint.x, y: intersectionPoint.y};
-          }
-        }
-      }
-    }
-    return closest;
-  }
-
-  testRefraction(x1, y1, angle, distance, lines) {
+  testReflection(x1, y1, angle, distance, lines, color) {
     var returnLines = [];
-    var x2 = x1 + Math.cos(angle) * distance;
-    var y2 = y1 + Math.sin(angle) * distance;
-    var intersection = this.findIntersectPoint(x1, y1, x2, y2, lines);
-    if (intersection) {
-      returnLines.push(this.addLineFromTo(
-        x1, y1,
-        intersection.x, intersection.y
-      ));
-    } else {
-      returnLines.push(this.addLineFromTo(
-        x1, y1,
-        x2, y2
-      ));
-    }
+    var reflectionLines = Physics.doLineReflections(x1, y1, angle, distance, lines);
+    reflectionLines.forEach((line) => {
+      returnLines.push(this.addLine(line, color));
+    });
 
     return returnLines;
   }
 
   runLineTester() {
     var lines = [
-      [40, 20, 240, 20],
-      [260, 40, 260, 240],
-      [20, 40, 20, 240]
+      Line(0, 0, 0, 9999999),
+      Line(0, 0, 9999999, 0),
+      Line(500, 9999999, 500, 0),
     ];
     for (var i = 0; i < lines.length; i++) {
       var line = lines[i];
-      this.addLineFromTo(line[0], line[1], line[2], line[3]);
+      this.addLine(line);
       this.forceRedraw();
     }
 
-    this.testRefraction(200, 300, Math.PI * 5 / 4.0, 400, lines);
+    for (var i = 20; i < 32 * 3 - 20; i++) {
+      this.testReflection(200, 300, Math.PI * i / 32.0, 750, lines, 0xffffff);
+    }
 
     this.forceRedraw();
   }
